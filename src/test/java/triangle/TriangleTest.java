4a644bd36c0dbb9eae6d4420b4cedc0371a3eb7b
Rishik Chand <rishik01@cs.washington.edu>
2026-02-13 20:02:23 -0800

    statement coverage

--- src/test/java/triangle/TriangleTest.java
+++ src/test/java/triangle/TriangleTest.java
@@ -6,6 +6,8 @@ import static triangle.Triangle.Type.*;
 
 import org.junit.jupiter.api.Test;
 
+import triangle.Triangle.Type;
+
 /** Test class for the Triangle implementation. */
 public class TriangleTest {
 
@@ -21,7 +23,14 @@ public class TriangleTest {
   }
 
   @Test
-  public void testTable() {
-    testTriangle(INVALID, 0, 0, 0);
-  }
+public void testTable() {
+    testTriangle(INVALID, 0, 1, 1);
+    testTriangle(SCALENE, 3, 4, 5);
+    testTriangle(INVALID, 1, 2, 3);
+    testTriangle(EQUILATERAL, 5, 5, 5);
+    testTriangle(ISOSCELES, 4, 4, 3);
+    testTriangle(INVALID, 2, 2, 5);
+    testTriangle(ISOSCELES, 4, 3, 4);
+    testTriangle(ISOSCELES, 3, 4, 4);
+}
 }
